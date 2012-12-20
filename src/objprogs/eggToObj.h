d63c3ffd986957e03fefeed7ece8cf7cf086d828
David Rose <david@ddrose.com>
2012-12-20 00:09:14 +0000

    loader.saveModel(), EggToObjConverter, etc.

--- src/objprogs/eggToObj.h
+++ src/objprogs/eggToObj.h
@@ -17,10 +17,7 @@
 
 #include "pandatoolbase.h"
 #include "eggToSomething.h"
-#include "pmap.h"
-
-class EggNode;
-class EggVertex;
+#include "eggToObjConverter.h"
 
 ////////////////////////////////////////////////////////////////////
 //       Class : EggToObj
@@ -36,38 +33,7 @@ protected:
   virtual bool handle_args(Args &args);
 
 private:
-  typedef pmap<LVecBase4d, int> UniqueVertices;
-  class VertexDef {
-  public:
-    VertexDef();
-    int _vert3_index;
-    int _vert4_index;
-    int _uv2_index;
-    int _uv3_index;
-    int _norm_index;
-  };
-  typedef pmap<EggVertex *, VertexDef> VertexMap;
-
-  void collect_vertices(EggNode *egg_node);
-  void write_faces(ostream &out, EggNode *egg_node);
-  void write_group_reference(ostream &out, EggNode *egg_node);
-  void get_group_name(string &group_name, EggGroupNode *egg_group);
-
-  void record_vertex(EggVertex *vertex);
-  int record_unique(UniqueVertices &unique, const LVecBase4d &vec);
-  int record_unique(UniqueVertices &unique, const LVecBase3d &vec);
-  int record_unique(UniqueVertices &unique, const LVecBase2d &vec);
-  int record_unique(UniqueVertices &unique, double pos);
-
-  void write_vertices(ostream &out, const string &prefix, int num_components, 
-                      const UniqueVertices &unique);
-
-private:
   bool _triangulate_polygons;
-
-  UniqueVertices _unique_vert3, _unique_vert4, _unique_uv2, _unique_uv3, _unique_norm;
-  VertexMap _vmap;
-  EggGroupNode *_current_group;
 };
 
 #endif
