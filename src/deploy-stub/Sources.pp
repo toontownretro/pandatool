3273539abc35358db8d27fd465126833283a32bc
Brian Lach <brianlach72@gmail.com>
2021-02-11 16:29:54 -0500

    deploy-stub Sources.pp

---
+++ src/deploy-stub/Sources.pp
@@ -0,0 +1,34 @@
+#define BUILD_DIRECTORY $[HAVE_PYTHON]
+
+#define USE_PACKAGES python
+
+#begin bin_target
+  #define TARGET deploy-stub
+
+  #define SOURCES \
+    deploy-stub.c \
+    $[if $[WINDOWS_PLATFORM],frozen_dllmain.c]
+
+  #if $[OSX_PLATFORM]
+    #define LFLAGS $[LFLAGS] -sectcreate __PANDA __panda /dev/null
+  #elif $[UNIX_PLATFORM]
+    #define LFLAGS $[LFLAGS] -Wl,-z,origin -rdynamic
+  #endif
+
+#end bin_target
+
+#begin bin_target
+  #define BUILD_TARGET $[or $[WINDOWS_PLATFORM],$[OSX_PLATFORM]]
+
+  #define TARGET deploy-stubw
+
+  #define SOURCES \
+    deploy-stub.c \
+    $[if $[WINDOWS_PLATFORM],frozen_dllmain.c]
+
+  #if $[OSX_PLATFORM]
+    #define LFLAGS $[LFLAGS] -sectcreate __PANDA __panda /dev/null
+    #define CFLAGS $[CFLAGS] -DMACOS_APP_BUNDLE=1
+  #endif
+
+#end bin_target
