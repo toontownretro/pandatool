58acfb1f0264a4221c743b4af04522cfe2867b9f
Bob <54867422+Bob-by@users.noreply.github.com>
2022-07-05 03:21:36 +0200

    Re-add updated softimage scripts

---
+++ src/softegg/Sources.pp
@@ -0,0 +1,26 @@
+#define BUILD_DIRECTORY $[HAVE_SOFTIMAGE]
+
+#begin lib_target
+  #define USE_PACKAGES softimage
+  #define TARGET softegg
+  #define LOCAL_LIBS \
+    converter pandatoolbase
+  #define OTHER_LIBS \
+    egg:c pandaegg:m \
+    linmath:c putil:c panda:m \
+    express:c pandaexpress:m \
+    dtoolutil:c dtoolbase:c prc interrogatedb dtool:m
+
+  #define BUILDING_DLL BUILDING_MISC
+  #define UNIX_SYS_LIBS \
+    m
+
+  #define SOURCES \
+    config_softegg.cxx config_softegg.h \
+    softEggGroupUserData.cxx softEggGroupUserData.I softEggGroupUserData.h \
+    softToEggConverter.cxx softToEggConverter.h \
+    soft2Egg.c \
+    softNodeTree.cxx softNodeTree.h \
+    softNodeDesc.cxx softNodeDesc.h
+
+#end lib_target
---
+++ src/softprogs/Sources.pp
@@ -0,0 +1,21 @@
+#define BUILD_DIRECTORY $[HAVE_SOFTIMAGE]
+
+#begin bin_target
+  #define TARGET softcvs
+  #define LOCAL_LIBS progbase
+  #define USE_PACKAGES openssl
+
+  #define OTHER_LIBS \ 
+    egg:c pandaegg:m \
+    pipeline:c event:c pstatclient:c panda:m \
+    dtoolbase:c pnmimage:c mathutil:c linmath:c putil:c express:c \
+    pandaexpress:m \
+    interrogatedb prc dtoolconfig:m \
+    dtoolutil:c dtoolbase:c dtool:m \
+    $[if $[WANT_NATIVE_NET],nativenet:c] \
+    $[if $[and $[HAVE_NET],$[WANT_NATIVE_NET]],net:c downloader:c]
+
+  #define SOURCES \
+    softCVS.cxx softCVS.h softFilename.cxx softFilename.h
+
+#end bin_target
