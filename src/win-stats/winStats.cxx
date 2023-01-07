db7141e0ef66381822ff6911388c600aae5b3667
Brian Lach <brianlach72@gmail.com>
2023-01-07 02:00:04 -0500

    :| :|

--- src/win-stats/winStats.cxx
+++ src/win-stats/winStats.cxx
@@ -40,11 +40,12 @@ int main(int argc, char *argv[]) {
 
   // Create the server window.
   WinStatsServer *server = new WinStatsServer;
-  if (server->parse_command_line(argc, argv) == ProgramBase::EC_failure) {
-    MessageBox(nullptr, "Failed to parse command-line options.",
-               "PStats Error", MB_OK | MB_ICONEXCLAMATION);
-    return 1;
-  }
+  server->parse_command_line(argc, argv);
+  //if (server->parse_command_line(argc, argv) == ProgramBase::EC_failure) {
+  //  MessageBox(nullptr, "Failed to parse command-line options.",
+  //             "PStats Error", MB_OK | MB_ICONEXCLAMATION);
+  //  return 1;
+  //}
 
   // Now get lost in the Windows message loop.
   MSG msg;
